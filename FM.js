106c2db2717f711023e274abb85f530f08bc652b
tironesr <73726058+tironesr@users.noreply.github.com>
2021-10-01 01:28:16 -0700

    Add files via upload

---
+++ FM.js
@@ -0,0 +1,13 @@
+setTimeout(function(){
+    document.getElementById('footer').classList.remove('hide');
+    }, 2000);
+    
+    setTimeout(function(){
+    document.getElementById('spinner').classList.remove('hide');
+    }, 2750);
+   
+   setTimeout(function(){
+      window.location.href = 'http://rascltest.byethost6.com/Biosketch.html';
+      }, 6250);
+   
+   
---
+++ connecting.js
@@ -0,0 +1,13 @@
+ setTimeout(function(){
+ document.getElementById('spinner').classList.remove('hide');
+ }, 250);
+
+setTimeout(function(){
+ document.getElementById('footer').classList.remove('hide');
+ }, 20000);
+ 
+    setTimeout(function(){
+       window.location.href = 'https://jsfiddle.net/6medg2fw/1/';
+    }, 45000);
+
+
